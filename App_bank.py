afa363756c059346daa52056b0b6752727aca409
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-16 23:55:31 +0100

    Update App_bank.py

--- App_bank.py
+++ App_bank.py
@@ -5,18 +5,8 @@ import streamlit as st
 
 number = st.slider("Pick a number",0,100)
 
-
-
-
-# ====================================================================
-# MENUS
-# ====================================================================
-
-
 st.sidebar.title("Menus")
-sidebar_selection = st.sidebar.radio(
-    'Select Menu:',
-    ['Overview' ,'Data Analysis', 'Model & Prediction','Prédire solvabilité client','Intéprétabilité'],
-)
+
+
                           
   
