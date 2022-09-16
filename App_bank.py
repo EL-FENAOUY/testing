3b61d39b57a0af4b0e3c891f6d16b5e5903b4022
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-17 00:28:10 +0100

    Update App_bank.py

--- App_bank.py
+++ App_bank.py
@@ -38,6 +38,72 @@ sidebar_selection = st.sidebar.radio(
 )
 
 
+if sidebar_selection == 'Overview':
+    selected_item =""
+    with st.spinner('Data load in progress...'):
+        time.sleep(2)
+    st.success('Data loaded')
+    show_data () 
+    show_overview () 
+
+if sidebar_selection == 'Data Analysis':
+    selected_item = st.sidebar.selectbox('Select Menu:', 
+                                ('Graphs', 'Distributions'))
+
+if sidebar_selection == 'Model & Prediction':
+    selected_item = st.sidebar.selectbox('Select Menu:', 
+                                    ( 'Prediction','Model'))
+
+if sidebar_selection == 'Prédire solvabilité client':
+    selected_item="predire_client"
+
+if sidebar_selection == 'Intéprétabilité':
+    selected_item =""
+    affiche_facteurs_influence()
+    
+
+seuil_risque = st.sidebar.slider("Seuil de Solvabilité", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+
+if selected_item == 'Data':
+    show_data ()  
+
+if selected_item == 'Solvency':
+    show_overview ()  
+
+if selected_item == 'Graphs':
+    #hist_graph()
+    is_educ_selected,is_statut_selected,is_income_selected = filter_graphs()
+    if(is_educ_selected=="oui"):
+        education_type()
+    if(is_statut_selected=="oui"):
+        statut_plot()
+    if(is_income_selected=="oui"):  
+        income_type()
+
+if selected_item == 'Distributions':
+    is_age_selected,is_incomdis_selected = filter_distribution()
+    if(is_age_selected=="oui"):
+        age_distribution()
+    if(is_incomdis_selected=="oui"):
+        revenu_distribution()
+
+    
+    
+
+if selected_item == 'Prediction':
+    show_client_predection()
+
+if selected_item == 'Model':
+    show_model_analysis()
+
+if selected_item == 'predire_client':
+    show_client_prediction()
+
+if selected_item == 'Fluance':
+    affiche_facteurs_influence()
+ 
+
+
 
 
 
