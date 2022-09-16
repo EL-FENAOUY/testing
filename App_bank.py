b6dca52e19ffb4cb3b7071a9edaec3b9092d922c
EL-FENAOUY <94041017+EL-FENAOUY@users.noreply.github.com>
2022-09-17 00:15:35 +0100

    Update App_bank.py

--- App_bank.py
+++ App_bank.py
@@ -10,6 +10,29 @@ st.sidebar.image(logo, width=240, caption=" Dashboard - Aide à la décision",
 
 number = st.slider("Pick a number",0,100)
 
+html_header="""
+    <head>
+        <title>Application Dashboard Crédit Score</title>
+        <meta charset="utf-8">
+        <meta name="keywords" content="Home Crédit Group, Dashboard, prêt, crédit score">
+        <meta name="description" content="Application de Crédit Score - dashboard">
+        <meta name="author" content="Loetitia Rabier">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>             
+    <h1 style="font-size:300%; color:Crimson; font-family:Arial"> Prêt à dépenser <br>
+        <h2 style="color:Gray; font-family:Georgia"> DASHBOARD</h2>
+        <hr style= "  display: block;
+          margin-top: 0;
+          margin-bottom: 0;
+          margin-left: auto;
+          margin-right: auto;
+          border-style: inset;
+          border-width: 1.5px;"/>
+     </h1>
+"""
+st.markdown('<style>body{background-color: #fbfff0}</style>',unsafe_allow_html=True)
+st.markdown(html_header, unsafe_allow_html=True)
+
 st.sidebar.title("Menus")
 sidebar_selection = st.sidebar.radio(
     'Select Menu:',
